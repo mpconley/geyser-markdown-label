1c8504ae7c442dbb5d46eb6f6540e6ade281948f
Michael Conley <sousesider@gmail.com>
2026-06-09 22:46:43 -0400

    Bump package metadata version to 0.1.3
    Signed-off-by: Michael Conley <sousesider@gmail.com>

--- package-src/config.lua
+++ package-src/config.lua
@@ -2,5 +2,5 @@ mpackage = [[geyser-markdown]]
 author = [[Mike Conley]]
 title = [[Geyser Markdown Label]]
 description = [[Adds Geyser.MarkdownLabel for rendering Markdown with a CommonMark/GFM-inspired feature set.]]
-version = [[0.1.0]]
+version = [[0.1.3]]
 created = "2026-06-09T00:00:00+00:00"
